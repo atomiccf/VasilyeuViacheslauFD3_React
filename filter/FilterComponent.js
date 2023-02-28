e333903061e4d8f17cf2893fb3fc14b10cf6cf2a
VasilyeuViacheslau <106859411+atomiccf@users.noreply.github.com>
2023-02-28 22:08:35 +0300

    add task filter

---
+++ filter/FilterComponent.js
@@ -0,0 +1,80 @@
+
+var FilterComponent = React.createClass({
+
+    displayName: 'FilterComponent',
+
+    propTypes: {
+        words: React.PropTypes.arrayOf(React.PropTypes.string.isRequired),
+
+    },
+
+
+    getInitialState() {
+        return {
+            isSort:false,
+            filterStr:'',
+            wordList:this.props.words
+
+        }
+
+
+
+    },
+    sortChecked:function (EO) {
+
+        this.setState({isSort:EO.target.checked},this.sortList);
+
+    },
+    filterChenged:function (EO) {
+
+    this.setState({filterStr:EO.target.value},this.sortList);
+
+    },
+
+    reset:function () {
+
+        this.setState( {isSort:false, filterStr:''},this.sortList)
+
+    },
+
+    sortList:function () {
+        let list = this.props.words.slice()
+        if(this.state.isSort) list.sort();
+
+        if (this.state.filterStr) list=list.filter(item => {
+            console.log(`${item}`)
+             return item.includes(this.state.filterStr)
+
+
+        })
+        this.setState({wordList:list});
+    },
+
+
+render: function () {
+
+    return   React.DOM.div( null,
+        React.DOM.div({className: 'container'},
+            React.createElement('input',{type:'checkbox', checked:this.state.isSort, onChange:this.sortChecked}),
+            React.createElement('input',{type:'text', value:this.state.filterStr,onChange:this.filterChenged}),
+            React.createElement('button',{type:'button',onClick:this.reset}, 'Reset'),
+            React.createElement('div',{className:'textarea'},this.state.wordList.join('\n')),
+        )
+
+    )
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+})
+
