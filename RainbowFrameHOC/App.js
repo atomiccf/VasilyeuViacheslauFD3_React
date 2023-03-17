2acb07bf30473caf3d20f4830389b25b076c9cbb
VasilyeuViacheslau <106859411+atomiccf@users.noreply.github.com>
2023-03-17 09:16:04 +0100

    add RainbowFrame  task

---
+++ RainbowFrameHOC/App.js
@@ -0,0 +1,29 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+import DoubleButton from "./components/DoubleButton";
+import withRainbowFrame from "./components/withRainbowFrame";
+
+let colors = ['red','orange', 'yellow','green', '#00BFFF', 'blue', 'purple'];
+
+     let cbPressed = (num) =>{
+
+          alert(`аргумент ${num}`)
+
+     }
+let FramedDoubleButton=withRainbowFrame(colors)(DoubleButton);
+
+ReactDOM.render(
+     <React.Fragment>
+          <DoubleButton caption1="однажды" caption2="пору" cbPressed={cbPressed} >в студёную зимнюю</DoubleButton>
+
+          <FramedDoubleButton caption1="я из лесу" caption2="мороз" cbPressed={cbPressed}>вышел, был сильный</FramedDoubleButton>
+     </React.Fragment>
+
+  /*  <RainbowFrame colors={colors}>
+      Hello!
+    </RainbowFrame>*/
+  , document.getElementById('container') 
+);
+
